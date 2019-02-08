f252ef6ce103bf2515759670632ebf968c18f0a7
ftadel <francois.tadel@mcgill.ca>
2019-02-08 17:14:50 +0100

    Bug fix: Computation of MNI coordinates

--- locateElectrodes.py
+++ locateElectrodes.py
@@ -2536,7 +2536,7 @@ class LocateElectrodes(QtGui.QDialog):
         # ===== MNI COORDINATES =====
         dict_plotsMNI = None
         if isComputeMni:
-            if (self.getT1preMniTransform() is None):
+            if (self.getT1preMniTransform() is not None):
                 if thread:
                     thread.emit(QtCore.SIGNAL("PROGRESS"), 5)
                     thread.emit(QtCore.SIGNAL("PROGRESS_TEXT"), "Computing MNI normalization...")
